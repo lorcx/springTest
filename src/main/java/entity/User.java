c69f30f949c690ede51a382d136a0e93b1e4a5ec
lorcx <740707954@qq.com>
2016-05-12 23:52:07 +0800

    Initial commit

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,25 @@
+package entity;
+
+/**
+ * Created by dell on 2016/5/12.
+ */
+public class User {
+    private String name;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
---
+++ src/main/java/test/Test1.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by dell on 2016/5/12.
+ */
+public class Test1 {
+    public static void main(String[] args) {
+    
+    }
+}
